2437283637b515fab6ee32489aeaedcd0a84324c
Albane <a.mathez@rubika-edu.com>
2021-02-15 16:51:57 +0100

    Amelioration camera
    mais sa ne marche pas

---
+++ MobileProject/Assets/Scripts/Hub/CameraControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Albane
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 dragOrigine;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PanCamera();
+    }
+
+    private void PanCamera()
+    {
+        if (Input.GetMouseButtonDown(0))
+            dragOrigine = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 difference = dragOrigine - cam.ScreenToWorldPoint(Input.mousePosition);
+
+            print("origin" + dragOrigine + "newPosition" + cam.ScreenToWorldPoint(Input.mousePosition) + " =difference " + difference);
+
+            cam.transform.position += difference;
+        }
+    }
+}
