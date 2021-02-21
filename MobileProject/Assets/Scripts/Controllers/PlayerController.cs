ff9b9736b8bb05d67b39ac6d261355ad8425b3e9
Noé <noe.blanc.2k@gmail.com>
2021-02-21 15:11:11 +0100

    Ajout Package A* (pathfinding)

--- MobileProject/Assets/Scripts/Controllers/PlayerController.cs
+++ MobileProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,25 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+
 
 //Script de Noé
 
 public class PlayerController : MonoBehaviour
 {
-    private NavMeshAgent navAgent;
+    
 
     void Start()
     {
-        navAgent = GetComponent<NavMeshAgent>();
+        
     }
 
     
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
-        {
-            navAgent.destination = Input.mousePosition;
-        }
+        
     }
 }
