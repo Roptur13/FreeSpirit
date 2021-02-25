eee2a310242a3b1e8c74da0f48ccbeb954fe8be3
Noé <noe.blanc.2k@gmail.com>
2021-02-25 19:47:28 +0100

    sélection de couleur dans l'inspector + déplacements clavier pour tests

--- MobileProject/Assets/Scripts/Controllers/PlayerController.cs
+++ MobileProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public enum characterColor { Red, Blue, Yellow, Purple, Orange, Green };
+    public characterColor color;
 
     void Start()
     {
@@ -17,6 +18,21 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Z))
+        {
+            transform.position += Vector3.up * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.position += Vector3.left * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime;
+        }
     }
 }
---
+++ MobileProject/Assets/Scripts/LevelDesign/DestructibleWall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Script de Noé
+
+public class DestructibleWall : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
