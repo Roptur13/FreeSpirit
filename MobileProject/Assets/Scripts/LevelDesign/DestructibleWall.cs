c05c4caaaf050149f90b9b633e7aac42c9af550a
Noé <noe.blanc.2k@gmail.com>
2021-02-27 17:09:25 +0100

    proto bloc destructible (à voir quand il y aura plusieurs persos)

--- MobileProject/Assets/Scripts/LevelDesign/DestructibleWall.cs
+++ MobileProject/Assets/Scripts/LevelDesign/DestructibleWall.cs
@@ -1,21 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 //Script de Noé
 
 public class DestructibleWall : MonoBehaviour
 {
-    
+    public GameObject player;
+    public Button destroyButton;
+    private PlayerController playerController;
+
+    [SerializeField]
+    private float distance;
+
     void Start()
     {
-        
+        destroyButton.gameObject.SetActive(false);
+        playerController = player.GetComponent<PlayerController>();
     }
 
     
     void Update()
     {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        
+        if(distance<=2f && playerController.color == PlayerController.characterColor.Red)
+        {
+            destroyButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            destroyButton.gameObject.SetActive(false);
+        }
+    }
+
+    public void BlockDestroy(Collider2D collider)
+    {
+        collider.enabled = false;
         
     }
 }
