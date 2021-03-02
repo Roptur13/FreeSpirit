75d9e477107d03cdada2ed3f0f88f578dcf3de94
Unknown <s.miquel@rubika-edu.com>
2021-03-02 19:50:49 +0100

    MovableObject Added

---
+++ MobileProject/Assets/Scripts/ScriptSamuel/MovableObjectScript/TopCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopCollider : MonoBehaviour
+{
+    public Button upButton;
+    public GameObject mainBody;
+
+    void Start()
+    {
+        upButton.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        upButton.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        upButton.gameObject.SetActive(false);
+    }
+
+    public void TopMove()
+    {
+        mainBody.transform.position += new Vector3(0, -0.5f, 0);
+        
+    }
+}
