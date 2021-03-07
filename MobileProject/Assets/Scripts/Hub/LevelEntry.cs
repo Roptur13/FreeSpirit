dd133970224915e218ff047de1c6493d0f31c50e
Albane MATHEZ <a.mathez@gmail.com>
2021-03-07 15:33:51 +0100

    Changement de scène possible depuis le hub

---
+++ MobileProject/Assets/Scripts/Hub/LevelEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Albane
+public class LevelEntry : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
