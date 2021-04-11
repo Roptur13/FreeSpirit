3a733f09d3619933d37dbf9615bfed73494da97c
Albane MATHEZ <a.mathez@gmail.com>
2021-04-11 15:00:37 +0200

    Solitude Meca

---
+++ MobileProject/Assets/Scripts/LevelDesign/RepairPlatform/PlatformDetectPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Albane & Noé
+//Detecte tout les elements
+
+public class PlatformDetectPlayer : MonoBehaviour
+{
+    public bool playerOnPlatform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerOnPlatform = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerOnPlatform = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerOnPlatform = false;
+    }
+}
---
+++ MobileProject/Assets/Scripts/LevelDesign/RepairPlatform/PlatformRepairDetection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlatformRepairDetection : MonoBehaviour
+{
+    public Button RepairButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RepairButton.gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        RepairButton.gameObject.SetActive(true);
+        Debug.Log("playerdetect");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        RepairButton.gameObject.SetActive(false);
+    }
+}
