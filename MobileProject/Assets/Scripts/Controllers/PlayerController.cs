1aa0685ab8eb074c7ca8162b540c606ca3eb57a4
Noé <noe.blanc.2k@gmail.com>
2021-05-08 18:58:08 +0100

    highlight + tentative de build mais problème de light seulement sur build

--- MobileProject/Assets/Scripts/Controllers/PlayerController.cs
+++ MobileProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -8,9 +8,23 @@ public class PlayerController : MonoBehaviour
 {
     public PlayerManager playerManager;
 
+    public GameObject highLight;
+
     public bool blue;
     public bool red;
     public bool yellow;
+
+    private void Update()
+    {
+        if (this.gameObject == playerManager.currentCharacter)
+        {
+            highLight.SetActive(true);
+        }
+        else
+        {
+            highLight.SetActive(false);
+        }
+    }
 
     private void OnMouseDown()
     {
