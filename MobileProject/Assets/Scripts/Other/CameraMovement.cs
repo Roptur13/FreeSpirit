6f5353f7c0c6ea8385402133a77e55729a92449a
Noé <noe.blanc.2k@gmail.com>
2021-05-18 13:44:37 +0100

    Déplacements de la caméra quand les persos atteignent un certain point

---
+++ MobileProject/Assets/Scripts/Other/CameraMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CollidersArrays
+{   
+    public CameraPositionTrigger[] colliders;
+}
+
+public class CameraMovement : MonoBehaviour
+{    
+    
+    public Vector3[] cameraPositions; //doit avoir une valeur de plus (la première) que colliderCategories, sa valeur importe peu car elle sert à éviter les out of range
+
+    public CollidersArrays[] colliderCategories;
+
+    [SerializeField]
+    private int triggersActivated;
+
+       
+    void Start()
+    {
+        triggersActivated = 0;           
+    }
+
+    
+    void Update()
+    {
+        for (int i = 0; i < colliderCategories.Length; i++)
+        {
+            for (int j = 0; j < colliderCategories[i].colliders.Length; j++)
+            {               
+
+                if (colliderCategories[i].colliders[j].activated == true)
+                {
+                    triggersActivated = triggersActivated + 1;
+                }                
+            }
+
+            if (triggersActivated >= colliderCategories[i].colliders.Length)
+            {
+                transform.position = Vector3.Lerp(transform.position, cameraPositions[i + 1], 0.1f);
+                triggersActivated = 0;
+            }
+            else
+            {
+                triggersActivated = 0;
+            }
+        }
+    }
+}
---
+++ MobileProject/Assets/Scripts/Other/CameraPositionTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraPositionTrigger : MonoBehaviour
+{
+    public bool activated;
+
+    private void Start()
+    {
+        activated = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        activated = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        activated = false;
+    }
+}
